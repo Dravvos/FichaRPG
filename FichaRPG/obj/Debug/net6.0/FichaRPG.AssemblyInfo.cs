2d24c84c8ce3fff02fb2ef184834ca558232c32b
Dravvos <daniddias53@gmail.com>
2024-10-30 15:08:05 -0300

    mudança porta http

--- FichaRPG/obj/Debug/net6.0/FichaRPG.AssemblyInfo.cs
+++ FichaRPG/obj/Debug/net6.0/FichaRPG.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b185ee6c5936effd81b8f148e6649e5c3a29c0b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c1ec644d0e14e63a2fcc5e296f90aefa84d831")]
 [assembly: System.Reflection.AssemblyProductAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
