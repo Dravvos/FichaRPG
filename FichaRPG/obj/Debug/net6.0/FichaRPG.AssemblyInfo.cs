e0054369ab5f8deb6fbf02356d20500b0d732aaa
Dravvos <daniddias53@gmail.com>
2024-11-02 02:46:24 -0300

    updat css reference

--- FichaRPG/obj/Debug/net6.0/FichaRPG.AssemblyInfo.cs
+++ FichaRPG/obj/Debug/net6.0/FichaRPG.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8113ef4b95b2e6c67a19591d88d17fa589efed0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a504ca84efe012ed1e27098c84b979e66eaa890a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FichaRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
